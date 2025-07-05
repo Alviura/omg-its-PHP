4e1c8e70eccae825757ca588962de963d436c01c
Alviura <alviura@outlook.com>
2025-07-05 20:40:02 +0300

    First commit

---
+++ php-practice.php
@@ -0,0 +1,15 @@
+<?php
+// this is a comment in PHP 
+
+// variables 
+$name = "Alvin";
+$age = 20;
+$is_cool = true;
+
+// Output
+echo "Hello my name is $name and I am $age years old.\n";
+
+// Data Types
+var_dump($is_cool); // shows data type and value
+
+?>
